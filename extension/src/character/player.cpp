349d5a064e917c1930cf3a3af2805c81cc684515
shindori fumiya <shindori238@gmail.com>
2023-08-11 01:43:30 +0900

    Add process input

--- extension/src/character/player.cpp
+++ extension/src/character/player.cpp
@@ -2,6 +2,8 @@
 
 #include <godot_cpp/core/class_db.hpp>
 
+#include "godot_cpp/classes/global_constants.hpp"
+#include "godot_cpp/classes/input.hpp"
 #include "godot_cpp/classes/input_event.hpp"
 #include "godot_cpp/variant/utility_functions.hpp"
 
@@ -14,30 +16,36 @@ Player::Player() {}
 Player::~Player() {}
 
 void Player::_physics_process(double delta) {
-  // if (Input::get_singleton()->is_action_pressed("Move_Right")) {
-  //   UtilityFunctions::print("HELLO");
-  // }
+  Input& inputSingleton = *Input::get_singleton();
+  if (inputSingleton.is_key_pressed(KEY_RIGHT)) {
+  }
+  if (inputSingleton.is_key_pressed(KEY_LEFT)) {
+  }
+  if (inputSingleton.is_key_pressed(KEY_UP)) {
+  }
+  if (inputSingleton.is_key_pressed(KEY_DOWN)) {
+  }
 }
 
 /**
  * プレイヤーの操作
  */
-void Player::_input(InputEvent *inputEvent) {
+void Player::_input(InputEvent* inputEvent) {
   bool isInputEventKey = inputEvent->is_class("InputEventKey");
-  if (isInputEventKey) {
-    if (inputEvent->is_action("Move_Right")) {
-      UtilityFunctions::print("Move_Right");
-    }
-    if (inputEvent->is_action("Move_Left")) {
-      UtilityFunctions::print("Move_Left");
-    }
-    if (inputEvent->is_action("Move_Up")) {
-      UtilityFunctions::print("Move_Up");
-    }
-    if (inputEvent->is_action("Move_Down")) {
-      UtilityFunctions::print("Move_Down");
-    }
-  }
+  // if (isInputEventKey) {
+  //   if (inputEvent->is_action("Move_Right")) {
+  //     UtilityFunctions::print("Move_Right");
+  //   }
+  //   if (inputEvent->is_action("Move_Left")) {
+  //     UtilityFunctions::print("Move_Left");
+  //   }
+  //   if (inputEvent->is_action("Move_Up")) {
+  //     UtilityFunctions::print("Move_Up");
+  //   }
+  //   if (inputEvent->is_action("Move_Down")) {
+  //     UtilityFunctions::print("Move_Down");
+  //   }
+  // }
 }
 
 #pragma clang diagnostic pop
