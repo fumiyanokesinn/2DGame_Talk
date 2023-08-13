ffdc253aec8d44e04daca8b8dd783f4d6186e042
shindori fumiya <shindori238@gmail.com>
2023-08-13 19:24:10 +0900

    fix player

--- extension/src/character/player.cpp
+++ extension/src/character/player.cpp
@@ -19,10 +19,7 @@ Player::Player() {
 
 Player::~Player() {}
 
-void Player::_ready() {
-  image = get_node<Sprite2D>(NodePath("Image"));
-  _animatedSprite2d = get_node<Sprite2D>(NodePath("Image"));
-}
+void Player::_ready() {}
 
 void Player::_physics_process(double delta) {
   velocity = Vector2(0.0f, 0.0f);
