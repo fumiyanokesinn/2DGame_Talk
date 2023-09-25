21f772b2a18f857a17da4570112f7e24488c249d
shindori fumiya <shindori238@gmail.com>
2023-09-26 00:37:16 +0900

    today working

--- extension/src/UI/ballonDialog.cpp
+++ extension/src/UI/ballonDialog.cpp
@@ -13,11 +13,29 @@ BallonDialog::BallonDialog() {}
 
 BallonDialog::~BallonDialog() {}
 
-void BallonDialog::_ready() { _name = get_node<Label>(NAME_PATH); }
+void BallonDialog::_ready() {
+  _name = get_node<Label>(NAME_PATH);
+  testChat();
+}
 
 void BallonDialog::testChat() {
-  //   std::ifstream f("extension\src\text\test.json");
-  //   json data = json::parse(f);
+  std::ifstream jsonFile("test.json");
+
+  if (!jsonFile.is_open()) {
+    std::cerr << "The JSON file could not be opened." << std::endl;
+    return;
+  }
+
+  // JSONデータをパースします
+  nlohmann::json jsonData;
+  jsonFile >> jsonData;
+
+  // JSONデータを操作したり、値を取得したりできます
+  std::string name = jsonData["name"];
+  // std::string name = j["test"]["name"];
+  // "name"の値を出力
+  std::cout << "name: " << std::endl;
+  // _name->set_text(name);
 }
 
 // 表示をONにする
--- extension/src/character/testNpc.h
+++ extension/src/character/testNpc.h
@@ -2,9 +2,9 @@
 #define TEST_NPC_H
 
 #include <godot_cpp/classes/animated_sprite2d.hpp>
+#include <godot_cpp/classes/area2d.hpp>
 #include <godot_cpp/classes/character_body2d.hpp>
 #include <godot_cpp/classes/sprite2d.hpp>
-
 using namespace godot;
 
 class TestNpc : public CharacterBody2D {
