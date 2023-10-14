460d4487da087f70e423ef337397c11da28d14d1
shindori fumiya <shindori238@gmail.com>
2023-10-14 16:01:06 +0900

    コメント修正

--- game/src/gd/util/dialogue/balloon_manager.gd
+++ game/src/gd/util/dialogue/balloon_manager.gd
@@ -3,14 +3,14 @@ extends Node
 # 既に会話しているか
 var isChat:bool = false
 
-## Show the example balloon
+# 会話バルーンを表示させる
 func show_dialogue_balloon(resource: DialogueResource,node: Node, title: String = "", extra_game_states: Array = []) -> void:
 	if(!isChat):
 #		会話していない場合,会話を実行
 		isChat = true;
 		var BalloonScene:Resource = load("res://src/tscn/dialogue/dialogue_ballon.tscn");
 		
-#		バルーンノードを追加
+#		バルーンノードを会話したキャラに追加
 		var balloon: Node2D = (BalloonScene).instantiate();
 		node.add_child(balloon);
 		balloon.position.y = -150;
