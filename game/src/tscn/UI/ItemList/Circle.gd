76df8a90733367fda4cb4358e7f639a985616812
shindori fumiya <shindori238@gmail.com>
2023-10-25 23:41:01 +0900

    サークルの移動を実装

--- game/src/tscn/UI/ItemList/Circle.gd
+++ game/src/tscn/UI/ItemList/Circle.gd
@@ -1,3 +1,20 @@
 extends Control
 
-var MARGIN_X:int = 125;
+const MARGIN_X:int = 125
+const MIN_MARGIN_X = 0
+const MAX_MARGIN_X = 1000
+
+var marginLeft:int = 0
+
+func _process(_delta:float)-> void:
+
+	if(Input.is_action_just_pressed("Circle_Move_Left")):
+		# Aキーを押下した場合、サークルを左に移動
+		if(marginLeft>MIN_MARGIN_X):
+			marginLeft-=MARGIN_X;
+		$MarginCircle.add_theme_constant_override("margin_left",marginLeft);
+	if(Input.is_action_just_pressed("Circle_Move_Right")):
+		# Dキーを押下した場合、サークルを右に移動
+		if(marginLeft<MAX_MARGIN_X):
+			marginLeft+=MARGIN_X;
+		$MarginCircle.add_theme_constant_override("margin_left",marginLeft);
